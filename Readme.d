c278c9860e99aae59158b92d4c0d0046b2a31d37
meharpalbasi <100152988+meharpalbasi@users.noreply.github.com>
2023-05-12 18:07:22 +0100

    Update Readme.d

--- Readme.d
+++ Readme.d
@@ -1,3 +1,3 @@
 ## Welcome to another Streamlit projects! 
 
-[You can use the application here.](https://meharpalbasi-equity-dashboard-equity-dashboard-v5uns1.streamlit.app)
+[You can use the application here.](https://meharpalbasi-equity-dashboard-equity-dashboard-v5uns1.streamlit.app/)
